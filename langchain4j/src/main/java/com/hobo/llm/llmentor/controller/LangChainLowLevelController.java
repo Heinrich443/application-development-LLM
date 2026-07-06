7b717eef421f68e356dd9f8ce8c741ff3eeaef99
Heinrich443 <nishiwosunza@163.com>
2026-07-06 11:08:21 +0800

    langchain4j low level chat memory

--- langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
+++ langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
@@ -1,6 +1,10 @@
 package com.hobo.llm.llmentor.controller;
 
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.openai.OpenAiChatModel;
@@ -10,6 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static dev.langchain4j.data.message.SystemMessage.systemMessage;
+import static dev.langchain4j.data.message.UserMessage.userMessage;
 
 @RestController
 @RequestMapping("/langchain")
@@ -69,5 +79,65 @@ public class LangChainLowLevelController {
             });
         });
         return flux;
+    }
+
+    @RequestMapping("/memory")
+    public String memory(HttpServletResponse response) {
+        List<ChatMessage> messages = new ArrayList<>();
+
+        //第一轮对话
+        messages.add(systemMessage("你是一个点餐助手"));
+        messages.add(userMessage("给我点一个汉堡，两个鸡腿，一杯可乐"));
+        AiMessage answer = chatModel.chat(messages).aiMessage();
+        System.out.println(answer);
+        System.out.println("======");
+
+        messages.add(answer);
+
+        //第二轮对话
+        messages.add(userMessage("刚才菜点多了，去掉一个鸡腿，再加一杯可乐吧?"));
+        AiMessage answer1 = chatModel.chat(messages).aiMessage();
+        System.out.println(answer1);
+        System.out.println("======");
+
+        messages.add(answer1);
+
+        //第三轮对话
+        messages.add(userMessage("我现在总共点了哪些东西？"));
+        AiMessage answer2 = chatModel.chat(messages).aiMessage();
+        System.out.println(answer2);
+        System.out.println("======");
+
+        return answer2.text();
+    }
+
+    @RequestMapping("/memory1")
+    public String memory1(HttpServletResponse response) {
+        ChatMemory messages = MessageWindowChatMemory.withMaxMessages(10);
+
+        //第一轮对话
+        messages.add(systemMessage("你是一个点餐助手"));
+        messages.add(userMessage("给我点一个汉堡，两个鸡腿，一杯可乐"));
+        AiMessage answer = chatModel.chat(messages.messages()).aiMessage();
+        System.out.println(answer);
+        System.out.println("======");
+
+        messages.add(answer);
+
+        //第二轮对话
+        messages.add(userMessage("刚才菜点多了，去掉一个鸡腿，再加一杯可乐吧?"));
+        AiMessage answer1 = chatModel.chat(messages.messages()).aiMessage();
+        System.out.println(answer1);
+        System.out.println("======");
+
+        messages.add(answer1);
+
+        //第三轮对话
+        messages.add(userMessage("我现在总共点了哪些东西？"));
+        AiMessage answer2 = chatModel.chat(messages.messages()).aiMessage();
+        System.out.println(answer2);
+        System.out.println("======");
+
+        return answer2.text();
     }
 }
