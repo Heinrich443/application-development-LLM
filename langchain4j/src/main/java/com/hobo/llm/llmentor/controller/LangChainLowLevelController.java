9ef3daf5d1f897729926859be8a5709ef27886f1
Heinrich443 <nishiwosunza@163.com>
2026-07-06 11:15:14 +0800

    langchain4j low level structured output

--- langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
+++ langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
@@ -5,6 +5,11 @@ import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.chat.request.ResponseFormatType;
+import dev.langchain4j.model.chat.request.json.JsonObjectSchema;
+import dev.langchain4j.model.chat.request.json.JsonSchema;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.openai.OpenAiChatModel;
@@ -139,5 +144,43 @@ public class LangChainLowLevelController {
         System.out.println("======");
 
         return answer2.text();
+    }
+
+    @RequestMapping("/structure")
+    public String structure() {
+
+        ResponseFormat responseFormat = ResponseFormat.builder()
+                .type(ResponseFormatType.JSON)
+                .jsonSchema(JsonSchema.builder()
+                        .name("Person")
+                        .rootElement(JsonObjectSchema.builder()
+                                .addStringProperty("name")
+                                .addIntegerProperty("age")
+                                .addNumberProperty("height")
+                                .addBooleanProperty("married")
+                                .required("name", "age", "height", "married")
+                                .build())
+                        .build())
+                .build();
+
+        ChatRequest chatRequest = ChatRequest.builder()
+                .responseFormat(responseFormat)
+                .messages(UserMessage.from("""
+                        John is 42 years old and lives an independent life.
+                        He stands 1.75 meters tall and carries himself with confidence.
+                        Currently unmarried, he enjoys the freedom to focus on his personal goals and interests.
+                        """))
+                .build();
+
+//        ChatRequest chatRequest = ChatRequest.builder()
+//                .responseFormat(responseFormat)
+//                .messages(UserMessage.from("""
+//                        John is 42 years old and lives an independent life.
+//                        He stands 1.75 meters tall and carries himself with confidence.
+//                        Currently unmarried, he enjoys the freedom to focus on his personal goals and interests.output in json format
+//                        """))
+//                .build();
+
+        return chatModel.chat(chatRequest).aiMessage().text();
     }
 }
