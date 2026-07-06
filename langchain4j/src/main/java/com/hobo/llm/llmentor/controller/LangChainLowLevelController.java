6d9f6bafefbc7a262b5a99fc7377898e9bb17094
Heinrich443 <nishiwosunza@163.com>
2026-07-06 11:01:25 +0800

    langchain4j low level sse

--- langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
+++ langchain4j/src/main/java/com/hobo/llm/llmentor/controller/LangChainLowLevelController.java
@@ -1,6 +1,8 @@
 package com.hobo.llm.llmentor.controller;
 
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,9 +26,48 @@ public class LangChainLowLevelController {
         return chatModel.chat(message);
     }
 
-//    @RequestMapping("/stream")
-//    public Flux<String> stream(String message, HttpServletResponse response) {
-//        response.setCharacterEncoding("UTF-8");
-//        return streamChatModel.chat();
-//    }
+    @RequestMapping("/stream")
+    public void stream(String message, HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        streamChatModel.chat(message, new StreamingChatResponseHandler() {
+            @Override
+            public void onPartialResponse(String partialResponse) {
+                System.out.println(partialResponse);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse chatResponse) {
+                System.out.println("=====end====");
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                System.out.println(error.toString());
+            }
+        });
+    }
+
+    @RequestMapping("/stream1")
+    public Flux<String> stream1(String message, HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        Flux<String> flux = Flux.create(sink -> {
+            streamChatModel.chat(message, new StreamingChatResponseHandler() {
+                @Override
+                public void onPartialResponse(String partialResponse) {
+                    sink.next(partialResponse);
+                }
+
+                @Override
+                public void onCompleteResponse(ChatResponse chatResponse) {
+                    sink.complete();
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    sink.error(error);
+                }
+            });
+        });
+        return flux;
+    }
 }
